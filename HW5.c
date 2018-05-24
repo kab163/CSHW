6ac2370be5510264b61c9bcd4c52b24ccab1a8cd
kristib <kristib@uoregon.edu>
2018-05-23 22:48:55 -0700

    adding everything

---
+++ HW5.c
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+
+using namespace std;
+
+int main() 
+{
+  /*
+ 		𝑃(𝑏, 𝑗) = max{𝑃(𝑏 − 𝑏 , 𝑗 − 1) + 𝑟 , 𝑃(𝑏, 𝑗 − 1)}
+		With a base function at 𝑃(·, 𝑗 − 1).
+
+ 		initialize all P(0, j) = 0 and all P(b, 0) = 0 for j = 0 to n:
+		for b = 1 to B:
+		if bj > b: P(b, j) = P(b, j – 1)
+		else: compute P(b, j) using recurrence relation above
+		return P(B, n)
+  */
+
+	return 0;
+}
